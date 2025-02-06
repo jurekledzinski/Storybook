695faaae1261b0d5e8f611aa352d12813618856c
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-02-06 13:05:47 +0100

    Add story backdrop to show and hide it

--- src/stories/overlays/backdrop/Backdrop.stories.tsx
+++ src/stories/overlays/backdrop/Backdrop.stories.tsx
@@ -1,5 +1,6 @@
-import { Meta, StoryObj } from '@storybook/react';
 import { Backdrop } from './Backdrop';
+import { Meta, StoryObj } from '@storybook/react';
+import { useState } from 'react';
 
 const meta: Meta<typeof Backdrop> = {
   component: Backdrop,
@@ -9,4 +10,22 @@ export default meta;
 
 type Story = StoryObj<typeof Backdrop>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  args: {
+    open: true,
+  },
+};
+
+export const State: Story = {
+  decorators: [
+    (Story) => {
+      const [open, setOpen] = useState(false);
+      return (
+        <div>
+          <button onClick={() => setOpen(true)}>Show backdrop</button>
+          <Story args={{ onClick: () => setOpen(false), open }} />
+        </div>
+      );
+    },
+  ],
+};
