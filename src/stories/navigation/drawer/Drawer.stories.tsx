909a4a88040046e575672c7a3115f6b883f76001
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-02-06 13:21:51 +0100

    Add stories for drawer with backdrop

--- src/stories/navigation/drawer/Drawer.stories.tsx
+++ src/stories/navigation/drawer/Drawer.stories.tsx
@@ -1,4 +1,5 @@
 import styles from './Drawer.module.css';
+import { Backdrop } from '../../overlays/backdrop/Backdrop';
 import { Drawer } from './Drawer';
 import { Meta, StoryObj } from '@storybook/react';
 import { useState } from 'react';
@@ -40,6 +41,7 @@ export const Right: Story = {
     },
   ],
 };
+
 export const Bottom: Story = {
   decorators: [
     (Story) => {
@@ -62,6 +64,49 @@ export const Left: Story = {
         <div className={styles.container}>
           <button onClick={() => setOpen((prev) => !prev)}>Show left</button>
           <Story args={{ open, direction: 'left' }} />
+        </div>
+      );
+    },
+  ],
+};
+
+export const DrawerBackdrop: Story = {
+  decorators: [
+    (Story) => {
+      const [open, setOpen] = useState(false);
+      return (
+        <div className={styles.container}>
+          <button onClick={() => setOpen((prev) => !prev)}>Show right</button>
+          <Backdrop onClick={() => setOpen(false)} open={open} />
+          <Story args={{ open, direction: 'right' }} />
+        </div>
+      );
+    },
+  ],
+};
+
+export const BackdropCloseOutside: Story = {
+  decorators: [
+    (Story) => {
+      const [open, setOpen] = useState(false);
+      return (
+        <div className={styles.container}>
+          <button onClick={() => setOpen(true)}>Show right</button>
+          <Backdrop open={open} />
+          <Story
+            args={{
+              open,
+              direction: 'right',
+              children: (
+                <button
+                  onClick={() => setOpen(false)}
+                  style={{ display: 'flex', justifySelf: 'flex-end' }}
+                >
+                  Close
+                </button>
+              ),
+            }}
+          />
         </div>
       );
     },
