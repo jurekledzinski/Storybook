fd4e57d1b98cf0af7b3411f1f70600d394664741
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-02-26 19:31:44 +0100

    Extend field story with form label and message components

--- src/stories/form elements/field/Field.stories.tsx
+++ src/stories/form elements/field/Field.stories.tsx
@@ -1,5 +1,8 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { Field } from './Field';
+import { Message } from '../../feedbacks/message/Message';
+import { Input } from '../input/Input';
+import { Label } from '../label/Label';
 
 const meta: Meta<typeof Field> = {
   title: 'Components/Form elements/Field',
@@ -7,9 +10,9 @@ const meta: Meta<typeof Field> = {
   args: {
     children: (
       <>
-        <label htmlFor="1">Name:</label>
-        <input type="text" />
-        <p>Message name</p>
+        <Label>Name:</Label>
+        <Input label="Name" type="text" variant="basic" placeholder="Name" />
+        <Message>Name is required</Message>
       </>
     ),
   },
@@ -22,7 +25,18 @@ type Story = StoryObj<typeof Field>;
 export const Default: Story = {
   decorators: [
     (Story) => (
-      <form>
+      <form
+        style={{
+          width: '300px',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '0.25rem',
+          padding: '0.5rem',
+        }}
+        noValidate
+      >
+        <Story />
+        <Story />
         <Story />
       </form>
     ),
