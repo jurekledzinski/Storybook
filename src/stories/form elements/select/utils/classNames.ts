eb80b265ea2772651e3b54c83cbf8fb2dfebb6e1
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-03-27 22:04:31 +0100

    Create utils folder for helpers in select component

---
+++ src/stories/form elements/select/utils/classNames.ts
@@ -0,0 +1,6 @@
+import stylesSelectPanel from '../components/select-panel/SelectPanel.module.css';
+
+export const selectPanelClassNames = () => ({
+  selectPanel: stylesSelectPanel,
+  selectPanelElement: stylesSelectPanel.selectPanelElement,
+});
