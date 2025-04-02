17529bd3f01c5c11e053ba58a7ea8d0ff73f2450
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-04-02 19:04:24 +0200

    Change imports to aliases in Field component

--- src/stories/form elements/field/Field.tsx
+++
@@ -1,6 +0,0 @@
-import styles from './Field.module.css';
-import { FiledProps } from './types';
-
-export const Field = ({ children }: FiledProps) => {
-  return <div className={styles.field}>{children}</div>;
-};
--- src/stories/form elements/field/types.ts
+++
@@ -1,3 +0,0 @@
-export type FiledProps = {
-  children: React.ReactNode;
-};
