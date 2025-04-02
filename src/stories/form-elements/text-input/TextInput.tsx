79cffebdf67ac819991872c41ac43e9876a659bb
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-04-02 19:12:18 +0200

    Changes import to aliases in text input component

---
+++ src/stories/form-elements/text-input/TextInput.tsx
@@ -0,0 +1,32 @@
+import styles from './TextInput.module.css';
+import { getInputProps } from './utils';
+import { Input, InputWrapper } from '@src/stories/form-elements/input';
+import { TextInputProps } from './types';
+
+export const TextInput = ({
+  as = 'input',
+  endIcon,
+  isError,
+  isPending,
+  startIcon,
+  type = 'text',
+  onClickEndIcon,
+  onClickStartIcon,
+  ...props
+}: TextInputProps) => {
+  return (
+    <div className={styles.textInput}>
+      <InputWrapper
+        isError={isError}
+        isPending={isPending}
+        onClickEndIcon={onClickEndIcon}
+        onClickStartIcon={onClickStartIcon}
+        {...(endIcon && { endIcon: endIcon[0] })}
+        {...(startIcon && { startIcon: startIcon[0] })}
+        {...props}
+      >
+        <Input as={as} {...getInputProps(as, type)} {...props} />
+      </InputWrapper>
+    </div>
+  );
+};
---
+++ src/stories/form-elements/text-input/index.ts
@@ -0,0 +1,4 @@
+export * from './TextInput.stories';
+export * from './TextInput';
+export * from './types';
+export * from './utils';
