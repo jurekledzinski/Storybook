dff76de25648ac7f044a850f7af102468a03d369
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-04-09 14:54:10 +0200

    Add isError prop return from helper function in text input

--- src/stories/form-elements/text-input/utils/helpers.ts
+++ src/stories/form-elements/text-input/utils/helpers.ts
@@ -3,8 +3,12 @@ import { TextInputProps } from '../types';
 type AsElement = TextInputProps['as'];
 type TypeInput = TextInputProps['type'];
 
-export const getInputProps = (as: AsElement, type: TypeInput) => {
-  if (as === 'input') return { type };
-  if (as === 'textarea') return { cols: 4, rows: 6 };
+export const getInputProps = (
+  as: AsElement,
+  type: TypeInput,
+  isError?: boolean
+) => {
+  if (as === 'input') return { type, isError };
+  if (as === 'textarea') return { cols: 4, rows: 6, isError };
   return {};
 };
