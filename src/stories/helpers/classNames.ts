ca18f9ed686fa64d4d677b3e26767725b7a1c349
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-04-11 14:42:02 +0200

    Create helper function to help generate classnames

--- src/stories/helpers/classNames.ts
+++ src/stories/helpers/classNames.ts
@@ -1,2 +1,31 @@
 export const classNames = (...classes: string[]) =>
   classes.filter(Boolean).join(' ');
+
+// export const generateClassNames = (
+//   baseClass: string,
+//   conditions: { [key: string]: boolean },
+//   additionalClasses: string[] = []
+// ) => {
+//   let classNames = [baseClass];
+
+//   Object.keys(conditions).forEach((key) => {
+//     if (conditions[key]) {
+//       classNames.push(`${baseClass}-${key}`);
+//     }
+//   });
+
+//   classNames = [...classNames, ...additionalClasses];
+
+//   return classNames.join(' ');
+// };
+
+export const generateClassNames = (
+  styles: Record<string, string>,
+  modifiers: Record<string, boolean>
+) => {
+  return Object.entries(modifiers)
+    .filter((item) => item[1])
+    .map(([key]) => styles[key])
+    .filter(Boolean)
+    .join(' ');
+};
