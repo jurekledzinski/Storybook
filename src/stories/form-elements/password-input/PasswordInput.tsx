b1601316a4535a9679589c8f56b358bd1da8277b
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-04-11 17:42:52 +0200

    Pass ref in password input component

--- src/stories/form-elements/password-input/PasswordInput.tsx
+++ src/stories/form-elements/password-input/PasswordInput.tsx
@@ -1,29 +1,33 @@
 import styles from './PasswordInput.module.css';
+import { forwardRef, Ref } from 'react';
 import { Input } from '../input/Input';
 import { InputWrapper } from '../input/components';
 import { PasswordInputProps } from './types';
 import { useState } from 'react';
 
-export const PasswordInput = ({
-  endIcon,
-  startIcon,
-  isPending,
-  ...props
-}: PasswordInputProps) => {
-  const [showPassword, setShowPassword] = useState(false);
+export const PasswordInput = forwardRef<
+  HTMLTextAreaElement | HTMLInputElement,
+  PasswordInputProps
+>(
+  (
+    { endIcon, startIcon, isPending, ...props },
+    ref: Ref<HTMLTextAreaElement | HTMLInputElement>
+  ) => {
+    const [show, setShow] = useState(false);
 
-  return (
-    <div className={styles.passwordInput}>
-      <InputWrapper
-        divider={true}
-        isPending={isPending}
-        onClickEndIcon={() => setShowPassword((prev) => !prev)}
-        {...(endIcon && { endIcon: showPassword ? endIcon[0] : endIcon[1] })}
-        {...(startIcon && { startIcon: startIcon[0] })}
-        {...props}
-      >
-        <Input type={showPassword ? 'text' : 'password'} {...props} />
-      </InputWrapper>
-    </div>
-  );
-};
+    return (
+      <div className={styles.passwordInput}>
+        <InputWrapper
+          divider={true}
+          isPending={isPending}
+          onClickEndIcon={() => setShow((prev) => !prev)}
+          {...(endIcon && { endIcon: show ? endIcon[0] : endIcon[1] })}
+          {...(startIcon && { startIcon: startIcon[0] })}
+          {...props}
+        >
+          <Input ref={ref} type={show ? 'text' : 'password'} {...props} />
+        </InputWrapper>
+      </div>
+    );
+  }
+);
