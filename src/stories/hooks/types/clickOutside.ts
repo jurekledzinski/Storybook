e3234f15cfcc67b4e78f8c2ab1b3c167ab85f3ea
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-04-12 13:53:19 +0200

    Add onLoadRefs prop in hook use close on click outside

--- src/stories/hooks/types/clickOutside.ts
+++ src/stories/hooks/types/clickOutside.ts
@@ -1,4 +1,4 @@
 export type UseCloseOnClickOutsideProps = {
   onClick: () => void;
-  refs: Record<string, React.RefObject<Node>>;
+  onLoadRefs: () => Record<string, React.RefObject<Node>>;
 };
--- src/stories/hooks/useCloseOnClickOutside.tsx
+++ src/stories/hooks/useCloseOnClickOutside.tsx
@@ -1,24 +1,27 @@
-import { useEffect } from 'react';
 import { UseCloseOnClickOutsideProps } from './types/clickOutside';
+import { useEffect } from 'react';
 
 export const useCloseOnClickOutside = ({
   onClick,
-  refs,
+  onLoadRefs,
 }: UseCloseOnClickOutsideProps) => {
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (!(event.target instanceof Node)) return;
+      const target = event.target as Node;
 
-      const isNotClickOutside = Object.values(refs).some((item) =>
-        item?.current?.contains(event.target as Node)
+      const refs = onLoadRefs();
+
+      const isClickOutside = Object.values(refs).some(
+        (ref) => ref && ref.current && ref?.current?.contains(target)
       );
 
-      if (!isNotClickOutside) onClick();
+      if (!isClickOutside) onClick();
     };
 
     document.addEventListener('mousedown', handleClickOutside);
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
-  }, [onClick, refs]);
+  }, [onClick, onLoadRefs]);
 };
