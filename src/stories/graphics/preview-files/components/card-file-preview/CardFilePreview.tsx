153723bce3348d22db493f8228bbdfde9f3bbe17
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-07-17 20:46:03 +0200

    Improve card preview file component

--- src/stories/graphics/preview-files/components/card-file-preview/CardFilePreview.tsx
+++ src/stories/graphics/preview-files/components/card-file-preview/CardFilePreview.tsx
@@ -1,8 +1,10 @@
 import styles from './CardFilePreview.module.css';
+import { Alert } from '@src/stories/feedbacks/alert';
 import { CardFilePreviewProps } from './types';
+import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
 import { getClassNamesCardFilePreview } from '../../utils';
-import { Image } from '@src/stories/graphics/image';
-import { useEffect } from 'react';
+import { Image, ImageContainer } from '@src/stories/graphics/image';
+import { useObjectUrl } from './hooks';
 
 export const CardFilePreview = ({
   file,
@@ -11,14 +13,32 @@ export const CardFilePreview = ({
   gridPlacement,
 }: CardFilePreviewProps) => {
   const classes = getClassNamesCardFilePreview(gridPlacement);
-
-  useEffect(() => {
-    return () => URL.revokeObjectURL(URL.createObjectURL(file));
-  }, [file]);
+  const objectUrl = useObjectUrl(file);
 
   return (
     <div className={classes.card}>
-      <Image src={URL.createObjectURL(file)} alt="image" />
+      <ImageContainer loader="skeleton">
+        {({ onLoad, onError, isError, isLoading }) => (
+          <>
+            {isError && !isLoading ? (
+              <Alert
+                color="negative"
+                icon={faTriangleExclamation}
+                message="Failed to load image"
+                fullWidth
+                size="size-xs"
+              />
+            ) : (
+              <Image
+                src={objectUrl}
+                onLoad={() => setTimeout(() => onLoad && onLoad(), 1000)}
+                onError={onError}
+                alt="image"
+              />
+            )}
+          </>
+        )}
+      </ImageContainer>
       <p className={classes.title}>{file.name}</p>
       <button className={styles.button} onClick={() => onRemove(index)}>
         x
@@ -26,3 +46,5 @@ export const CardFilePreview = ({
     </div>
   );
 };
+
+// style={{ aspectRatio: '1/1' }}
--- src/stories/graphics/preview-files/utils/classNames.ts
+++ src/stories/graphics/preview-files/utils/classNames.ts
@@ -1,34 +1,23 @@
 import stylesCardFile from '../components/card-file-preview/CardFilePreview.module.css';
 import stylesPreview from '../PreviewFiles.module.css';
-import { generateClassNames } from '@src/stories/helpers';
+import { classNames, generateClassNames } from '@src/stories/helpers';
 import { PreviewFileProps } from '../types';
 
 type Placement = Pick<PreviewFileProps, 'gridPlacement'>['gridPlacement'];
 
-// export const getClassNamesPreviewFiles = (gridPlacement?: Placement) =>
-//   classNames(
-//     stylesPreview.grid,
-//     generateClassNames(stylesPreview, { [`${gridPlacement}`]: true })
-//   );
-
 export const getClassNamesPreviewFiles = (gridPlacement?: Placement) =>
-  generateClassNames(stylesPreview, { [`${gridPlacement}`]: true });
-
-// export const getClassNamesCardFilePreview = (gridPlacement?: Placement) => ({
-//   card: classNames(
-//     stylesCardFile.card,
-//     generateClassNames(
-//       { ...stylesPreview, ...stylesCardFile },
-//       { [`card-${gridPlacement}`]: true }
-//     )
-//   ),
-//   title: stylesCardFile.title,
-// });
+  classNames(
+    stylesPreview.grid,
+    generateClassNames(stylesPreview, { [`${gridPlacement}`]: true })
+  );
 
 export const getClassNamesCardFilePreview = (gridPlacement?: Placement) => ({
-  card: generateClassNames(
-    { ...stylesPreview, ...stylesCardFile },
-    { [`card-${gridPlacement}`]: true }
+  card: classNames(
+    stylesCardFile.card,
+    generateClassNames(
+      { ...stylesPreview, ...stylesCardFile },
+      { [`card-${gridPlacement}`]: true }
+    )
   ),
   title: stylesCardFile.title,
 });
