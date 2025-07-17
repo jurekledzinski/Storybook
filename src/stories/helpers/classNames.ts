8594bf32bd0a33ca90d6fdc3adf888c088c6421b
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-07-17 20:47:37 +0200

    Remove commented code from helpers classNames file

--- src/stories/helpers/classNames.ts
+++ src/stories/helpers/classNames.ts
@@ -1,29 +1,10 @@
 export const classNames = (...classes: string[]) =>
   classes.filter(Boolean).join(' ');
 
-// export const generateClassNames = (
-//   baseClass: string,
-//   conditions: { [key: string]: boolean },
-//   additionalClasses: string[] = []
-// ) => {
-//   let classNames = [baseClass];
-
-//   Object.keys(conditions).forEach((key) => {
-//     if (conditions[key]) {
-//       classNames.push(`${baseClass}-${key}`);
-//     }
-//   });
-
-//   classNames = [...classNames, ...additionalClasses];
-
-//   return classNames.join(' ');
-// };
-
 export const generateClassNames = (
   styles: Record<string, string>,
   modifiers: Record<string, boolean>
 ) => {
-  //   console.log('modifiers', modifiers);
   return Object.entries(modifiers)
     .filter((item) => item[1])
     .map(([key]) => styles[key])
