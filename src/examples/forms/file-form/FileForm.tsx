3fa3a1dc2d8681181d53b1efeab184851de5b8f1
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-07-18 10:35:31 +0200

    Create folder examples and add form file

---
+++ src/examples/forms/file-form/FileForm.tsx
@@ -0,0 +1,126 @@
+import styles from './FileForm.module.css';
+import { Button } from '@src/stories/buttons/button';
+import { ButtonGroup } from '@src/stories/buttons/button-group';
+import { faImage } from '@fortawesome/free-solid-svg-icons';
+import { Field } from '@src/stories/form-elements/field';
+import { Form } from '@src/stories/form-elements/form';
+import { Heading } from '@src/stories/typography/heading';
+import { Icon } from '@src/stories/graphics/icon';
+import { Input } from '@src/stories/form-elements/input';
+import { Inputs } from './types';
+import { Label } from '@src/stories/form-elements/label';
+import { Message } from '@src/stories/feedbacks/message';
+import { PreviewFiles } from '@src/stories/graphics/preview-files';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import {
+  FileInput,
+  useValidateFiles,
+} from '@src/stories/form-elements/file-input';
+
+export const FileForm = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    formState: { errors },
+  } = useForm<Inputs>({ defaultValues: { files: [], name: '' } });
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log('Submit', data);
+
+  const {
+    checkFileType,
+    checkMaxAmountFiles,
+    checkMaxSize,
+    checkRequiredFiles,
+  } = useValidateFiles({
+    allowTypes: ['image/jpeg'],
+    maxSize: [3, 'MB'],
+    maxAmount: 2,
+  });
+
+  return (
+    <div className={styles.container}>
+      <Form onSubmit={handleSubmit(onSubmit)} noValidate>
+        <Field>
+          <Label>Name:</Label>
+          <Input
+            isError={undefined}
+            {...register('name', {
+              required: { message: 'Name is required', value: true },
+            })}
+          />
+          <Message variant="error">{errors.name?.message}</Message>
+        </Field>
+
+        <Field>
+          <Label>Upload file:</Label>
+          <FileInput
+            accept=".jpg"
+            label="Add images"
+            multiple
+            {...register('files', {
+              validate: (files) => {
+                const type = checkFileType(files);
+                const maxAmount = checkMaxAmountFiles(files);
+                const maxSize = checkMaxSize(files);
+                const required = checkRequiredFiles(files);
+
+                if (type !== true) return type;
+                if (maxAmount !== true) return maxAmount;
+                if (maxSize !== true) return maxSize;
+                if (required !== true) return required;
+
+                return true;
+              },
+            })}
+            // onChange={(e) => {
+            // Przykład jak dodawać więcej zdjęć i nie usuwać wszystkich tylko te które usuwamy
+            //   const files = e.target.files;
+            //   if (!files) return;
+            //   setValue('files', [...watch('files'), ...Array.from(files)], {
+            //     shouldValidate: true,
+            //     shouldDirty: false,
+            //   });
+            // }}
+          />
+          <Message variant="error">{errors.files?.message}</Message>
+        </Field>
+
+        <Field>
+          <Label>Preview files:</Label>
+          <PreviewFiles
+            images={[...watch('files')]}
+            gridPlacement="column"
+            onRemove={(index) => {
+              setValue(
+                'files',
+                [...watch('files')].filter((_, i) => i !== index),
+                { shouldValidate: true, shouldDirty: false }
+              );
+            }}
+          >
+            <>
+              <Icon
+                icon={faImage}
+                size="4x"
+                style={{
+                  position: 'absolute',
+                  transform: 'translate(-50%,-50%)',
+                  left: '50%',
+                  top: '50%',
+                }}
+              />
+              <Heading fw="fw-900" level={4} className={styles.heading}>
+                No Images
+              </Heading>
+            </>
+          </PreviewFiles>
+        </Field>
+        <ButtonGroup marginTop={16} fullWidth>
+          <Button label="Submit" fullWidth color="success" size="size-lg" />
+        </ButtonGroup>
+      </Form>
+    </div>
+  );
+};
---
+++ src/examples/forms/file-form/index.ts
@@ -0,0 +1,2 @@
+export * from './FileForm';
+export * from './types';
