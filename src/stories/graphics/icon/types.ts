eccbf5b26ee73692e0fcbd2a6fa7c04521543db3
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-07-18 10:47:40 +0200

    Add className prop to Icon component

--- src/stories/graphics/icon/types.ts
+++ src/stories/graphics/icon/types.ts
@@ -2,5 +2,6 @@ import { Color } from '@src/stories/types';
 import { FontAwesomeIconProps } from '@fortawesome/react-fontawesome';
 
 export interface IconProps extends FontAwesomeIconProps {
+  className?: string;
   color?: Color | 'info';
 }
