228c5a2ee9de5fce55c69639dd12a598fdec2530
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-07-18 10:48:33 +0200

    Update getClassIcon component

--- src/stories/graphics/icon/utils/classNames.ts
+++ src/stories/graphics/icon/utils/classNames.ts
@@ -2,6 +2,6 @@ import styles from '../Icon.module.css';
 import { classNames } from '@src/stories/helpers';
 import { Color } from '@src/stories/types';
 
-export const getClassIcon = (color?: Color | 'info') => {
-  return classNames(styles.icon, styles[color as keyof typeof styles]);
+export const getClassIcon = (className?: string, color?: Color | 'info') => {
+  return classNames(styles.icon, styles[color ?? ''], className ?? '');
 };
