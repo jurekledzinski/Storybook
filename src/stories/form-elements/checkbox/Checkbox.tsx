e8a817c13c229ea2453a05b79c97e487db1701b1
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-07-20 17:25:55 +0200

    Fix sizes style checkbox

--- src/stories/form-elements/checkbox/Checkbox.tsx
+++ src/stories/form-elements/checkbox/Checkbox.tsx
@@ -1,15 +1,18 @@
+import { forwardRef } from 'react';
 import { CheckboxInput } from './components';
 import { CheckboxProps } from './types';
 import { getClassCheckbox } from './utils';
 
-export const Checkbox = ({ children, ...props }: CheckboxProps) => {
-  const { color, disabled, readOnly, size, variant } = props;
-  const classes = getClassCheckbox(color, disabled, readOnly, size, variant);
+export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
+  ({ children, ...props }, ref) => {
+    const { color, disabled, readOnly, size, variant } = props;
+    const classes = getClassCheckbox(color, disabled, readOnly, size, variant);
 
-  return (
-    <label className={classes.label} htmlFor={props.id}>
-      <CheckboxInput {...props} />
-      {children}
-    </label>
-  );
-};
+    return (
+      <label className={classes.label} htmlFor={props.id}>
+        <CheckboxInput {...props} ref={ref} />
+        {children}
+      </label>
+    );
+  }
+);
