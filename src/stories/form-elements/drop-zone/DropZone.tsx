aa8bb05e2b1af375d490aabddc10012bd9414462
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-07-20 16:13:15 +0200

    Add useImperativeHandke in drop zone to provider ref to parent

--- src/stories/form-elements/drop-zone/DropZone.tsx
+++ src/stories/form-elements/drop-zone/DropZone.tsx
@@ -1,36 +1,36 @@
+import { classNames } from '@src/stories/helpers';
 import { dropZoneClassNames } from './utils/classNames';
 import { DropZoneProps } from './types';
+import { forwardRef, useImperativeHandle } from 'react';
 import { useControlDrop, useDropZone } from './hooks';
-import { classNames } from '@src/stories/helpers';
 
-export const DropZone = ({
-  children,
-  className,
-  title,
-  onDrop,
-}: DropZoneProps) => {
-  const { isEnter, refZone, onEnter, onLeave } = useDropZone();
-  const { onDragEnter, onDragOver, onDragLeave } = useControlDrop({
-    isEnter,
-    onEnter,
-    onLeave,
-    refZone,
-  });
+export const DropZone = forwardRef<HTMLDivElement, DropZoneProps>(
+  ({ children, className, title, onDrop }, ref) => {
+    const { isEnter, refZone, onEnter, onLeave } = useDropZone();
+    const { onDragEnter, onDragOver, onDragLeave } = useControlDrop({
+      isEnter,
+      onEnter,
+      onLeave,
+      refZone,
+    });
 
-  const classes = dropZoneClassNames(isEnter);
+    useImperativeHandle(ref, () => refZone.current!);
 
-  return (
-    <div
-      className={classNames(classes.zone, className)}
-      onDragOver={onDragOver}
-      onDragEnter={onDragEnter}
-      onDragLeave={onDragLeave}
-      onDrop={onDrop}
-      onDropCapture={() => onLeave()}
-      ref={refZone}
-    >
-      <h4 className={classes.title}>{title}</h4>
-      {children}
-    </div>
-  );
-};
+    const classes = dropZoneClassNames(isEnter);
+
+    return (
+      <div
+        className={classNames(classes.zone, className)}
+        onDragOver={onDragOver}
+        onDragEnter={onDragEnter}
+        onDragLeave={onDragLeave}
+        onDrop={onDrop}
+        onDropCapture={() => onLeave()}
+        ref={refZone}
+      >
+        <h4 className={classes.title}>{title}</h4>
+        {children}
+      </div>
+    );
+  }
+);
