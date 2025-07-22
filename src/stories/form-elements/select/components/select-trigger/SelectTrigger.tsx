f0da50de4ccfd504fa51b0c9b6e0322a7affa2eb
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-07-22 10:37:44 +0200

    Fix keyboard issue on select trigger

--- src/stories/form-elements/select/components/select-trigger/SelectTrigger.tsx
+++ src/stories/form-elements/select/components/select-trigger/SelectTrigger.tsx
@@ -1,4 +1,5 @@
 import { capitalizeFirstLetter } from '@src/stories/helpers';
+import { MouseEvent } from 'react';
 import { SelectTriggerProps } from './types';
 import { useAriaAttributes } from '@src/stories/hooks';
 import { usePopOver } from '@src/stories/overlays/pop-over';
@@ -11,27 +12,39 @@ import {
 export const SelectTrigger = ({ endIcon }: SelectTriggerProps) => {
   const { onToggle, open, registerTriggerRef } = usePopOver();
   const { isError, label, size, value, variant } = useSelect();
-  const { selectTriggerA11y } = useAriaAttributes();
 
+  const isOpen = open['root'];
+  const a11y = useAriaAttributes().selectTriggerA11y(isOpen, `Choose ${label}`);
   const classes = getClassNamesInput({ variant, size, isError });
+
+  const onClick = (e: MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    if (onToggle) onToggle('root');
+  };
 
   return (
     <InputWrapper
-      endIcon={open['root'] ? endIcon[0] : endIcon[1]}
+      endIcon={isOpen ? endIcon[0] : endIcon[1]}
       isError={isError}
-      onClickEndIcon={() => onToggle('root')}
+      onClickEndIcon={onClick}
       ref={(node) => registerTriggerRef(node, 'root')}
       size={size}
       variant={variant}
       divider={true}
-      {...selectTriggerA11y(open['root'], `Choose ${label}`)}
+      {...a11y}
     >
-      <fieldset className={classes.fieldset} onClick={() => onToggle('root')}>
+      <fieldset className={classes.fieldset} onClick={onClick}>
         <input
           className={classes.input}
           type="text"
           readOnly={true}
           value={capitalizeFirstLetter(value)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              e.preventDefault();
+              onToggle('root');
+            }
+          }}
         />
         {variant !== 'basic' && (
           <legend className={classes.legend}>{label}</legend>
