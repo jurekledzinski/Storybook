b6d9cfdd8ef3a8cb54a67a9a2080a9076a84d9f4
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-07-22 15:37:22 +0200

    Refactor select trigger types

--- src/stories/form-elements/select/components/select-trigger/types.ts
+++ src/stories/form-elements/select/components/select-trigger/types.ts
@@ -1,8 +1,5 @@
 import { Icons } from '@src/stories/types';
-import { KeyboardEvent, MouseEvent } from 'react';
 
 export type SelectTriggerProps = {
   endIcon: Icons;
 };
-
-export type Events = MouseEvent<HTMLElement> | KeyboardEvent<HTMLInputElement>;
