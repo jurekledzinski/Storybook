9cfdd347483e2b46452d7994e18473a23eaae343
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-07-22 10:38:08 +0200

    Refactor select provider

--- src/stories/form-elements/select/store/SelectProvider.tsx
+++ src/stories/form-elements/select/store/SelectProvider.tsx
@@ -1,10 +1,35 @@
-import { SelectProviderProps } from './types';
 import { SelectContext } from './contextSelect';
+import { SelectProviderProps } from './types';
+import { useMemo } from 'react';
+import {
+  useArrowNavigation,
+  useKeyPress,
+  onDown,
+  onUp,
+  onEnter,
+  onEscape,
+  onTab,
+} from '@src/stories/hooks';
 
 export const SelectProvider = ({ children, ...props }: SelectProviderProps) => {
+  const { onKeyArrow } = useArrowNavigation({ onDown, onUp });
+  const { onKeyPress } = useKeyPress({
+    onSelectItem: (id) => props.value.onChange && props.value.onChange(id),
+    onEnter,
+    onEscape,
+    onTab,
+  });
+
+  const values = useMemo(
+    () => ({
+      onKeyArrow,
+      onKeyPress,
+      ...props.value,
+    }),
+    [onKeyArrow, onKeyPress, props.value]
+  );
+
   return (
-    <SelectContext.Provider value={props.value}>
-      {children}
-    </SelectContext.Provider>
+    <SelectContext.Provider value={values}>{children}</SelectContext.Provider>
   );
 };
