3e49596cd03b1168eb21e50a54fe75897231ed79
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-07-22 10:58:56 +0200

    Refactor select trigger component

--- src/stories/form-elements/select/components/select-trigger/SelectTrigger.tsx
+++ src/stories/form-elements/select/components/select-trigger/SelectTrigger.tsx
@@ -1,6 +1,6 @@
 import { capitalizeFirstLetter } from '@src/stories/helpers';
-import { MouseEvent } from 'react';
-import { SelectTriggerProps } from './types';
+import { Events, SelectTriggerProps } from './types';
+import { KeyboardEvent } from 'react';
 import { useAriaAttributes } from '@src/stories/hooks';
 import { usePopOver } from '@src/stories/overlays/pop-over';
 import { useSelect } from '../../store';
@@ -17,34 +17,33 @@ export const SelectTrigger = ({ endIcon }: SelectTriggerProps) => {
   const a11y = useAriaAttributes().selectTriggerA11y(isOpen, `Choose ${label}`);
   const classes = getClassNamesInput({ variant, size, isError });
 
-  const onClick = (e: MouseEvent<HTMLElement>) => {
+  const handleToggle = (e: Events) => {
     e.preventDefault();
     if (onToggle) onToggle('root');
+  };
+
+  const onKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') handleToggle(e);
   };
 
   return (
     <InputWrapper
       endIcon={isOpen ? endIcon[0] : endIcon[1]}
       isError={isError}
-      onClickEndIcon={onClick}
+      onClickEndIcon={handleToggle}
       ref={(node) => registerTriggerRef(node, 'root')}
       size={size}
       variant={variant}
       divider={true}
       {...a11y}
     >
-      <fieldset className={classes.fieldset} onClick={onClick}>
+      <fieldset className={classes.fieldset} onClick={handleToggle}>
         <input
           className={classes.input}
           type="text"
-          readOnly={true}
+          readOnly
           value={capitalizeFirstLetter(value)}
-          onKeyDown={(e) => {
-            if (e.key === 'Enter') {
-              e.preventDefault();
-              onToggle('root');
-            }
-          }}
+          onKeyDown={onKeyDown}
         />
         {variant !== 'basic' && (
           <legend className={classes.legend}>{label}</legend>
--- src/stories/form-elements/select/components/select-trigger/types.ts
+++ src/stories/form-elements/select/components/select-trigger/types.ts
@@ -1,5 +1,8 @@
 import { Icons } from '@src/stories/types';
+import { KeyboardEvent, MouseEvent } from 'react';
 
 export type SelectTriggerProps = {
   endIcon: Icons;
 };
+
+export type Events = MouseEvent<HTMLElement> | KeyboardEvent<HTMLInputElement>;
