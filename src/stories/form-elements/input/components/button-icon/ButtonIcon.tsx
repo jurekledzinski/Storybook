84d8346549e4693bd5a7318964968b402d6d1cb1
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-07-22 15:32:17 +0200

    Remove tabindex 0 on button icon component

--- src/stories/form-elements/input/components/button-icon/ButtonIcon.tsx
+++ src/stories/form-elements/input/components/button-icon/ButtonIcon.tsx
@@ -5,7 +5,7 @@ import { Icon } from '@src/stories/graphics/icon';
 export const ButtonIcon = forwardRef<HTMLButtonElement, ButtonIconProps>(
   ({ className, icon, onClick }, ref: Ref<HTMLButtonElement>) => {
     return (
-      <button ref={ref} className={className} tabIndex={0} onClick={onClick}>
+      <button ref={ref} className={className} onClick={onClick}>
         <Icon icon={icon} size="1x" />
       </button>
     );
