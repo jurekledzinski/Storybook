b0b112b50a691fd25895798dcc3bdb0fb59045f1
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-07-25 09:59:30 +0200

    Remove unnecessary classNames function from helper utils in input component

--- src/stories/form-elements/input/utils/classNames.ts
+++ src/stories/form-elements/input/utils/classNames.ts
@@ -1,5 +1,5 @@
 import stylesInput from '../styles/Styles.module.css';
-import { classNames, generateClassNames } from '@src/stories/helpers';
+import { generateClassNames } from '@src/stories/helpers';
 import { GetClassNamesInput, GetClassNamesInputWrapper } from './types';
 
 export const getClassNamesInputWrapper: GetClassNamesInputWrapper = (
@@ -20,91 +20,77 @@ export const getClassNamesInputWrapper: GetClassNamesInputWrapper = (
   } = params;
 
   return {
-    inputWrapper: classNames(
-      stylesInput.wrapper,
-      generateClassNames(stylesInput, {
-        ['focused']: Boolean(className),
-        [`wrapper-${variant}`]: Boolean(variant),
-        'wrapper-disabled': Boolean(disabled),
-        'wrapper-inValid': isError === true,
-        'wrapper-valid': isError === false,
-        'wrapper-startIcon': Boolean(startIcon),
-        'wrapper-endIcon': Boolean(endIcon) || Boolean(isPending),
-        'wrapper-divider':
-          (Boolean(divider) && variant === 'basic') ||
-          (Boolean(divider) && variant === 'outlined'),
-        'wrapper-text': as === 'input',
-        'wrapper-area': as === 'textarea',
-      })
-    ),
-    startIcon: classNames(
-      stylesInput.startIcon,
-      generateClassNames(stylesInput, {
-        [`startIcon-${size}`]: Boolean(size),
-        [`startIcon-${variant}`]: Boolean(variant),
-        'startIcon-disabled': Boolean(disabled),
-        'startIcon-read-only': Boolean(readOnly),
-      })
-    ),
-    endIcon: classNames(
-      stylesInput.endIcon,
-      generateClassNames(stylesInput, {
-        [`endIcon-${size}`]: Boolean(size),
-        [`endIcon-${variant}`]: Boolean(variant),
-        'endIcon-disabled': Boolean(disabled),
-        'endIcon-read-only': Boolean(readOnly),
-      })
-    ),
-    statusIcon: classNames(
-      stylesInput.endIcon,
-      generateClassNames(stylesInput, {
-        [`endIcon-${size}`]: Boolean(size),
-        [`endIcon-${variant}`]: Boolean(variant),
-        'endIcon-disabled': Boolean(disabled),
-        'endIcon-inValid': isError === true,
-        'endIcon-valid': isError === false,
-      })
-    ),
+    inputWrapper: generateClassNames(stylesInput, {
+      wrapper: true,
+      focused: Boolean(className),
+      [`wrapper-${variant}`]: Boolean(variant),
+      'wrapper-disabled': Boolean(disabled),
+      'wrapper-inValid': isError === true,
+      'wrapper-valid': isError === false,
+      'wrapper-startIcon': Boolean(startIcon),
+      'wrapper-endIcon': Boolean(endIcon) || Boolean(isPending),
+      'wrapper-divider':
+        (Boolean(divider) && variant === 'basic') ||
+        (Boolean(divider) && variant === 'outlined'),
+      'wrapper-text': as === 'input',
+      'wrapper-area': as === 'textarea',
+    }),
+
+    startIcon: generateClassNames(stylesInput, {
+      startIcon: true,
+      [`startIcon-${size}`]: Boolean(size),
+      [`startIcon-${variant}`]: Boolean(variant),
+      'startIcon-disabled': Boolean(disabled),
+      'startIcon-read-only': Boolean(readOnly),
+    }),
+
+    endIcon: generateClassNames(stylesInput, {
+      endIcon: true,
+      [`endIcon-${size}`]: Boolean(size),
+      [`endIcon-${variant}`]: Boolean(variant),
+      'endIcon-disabled': Boolean(disabled),
+      'endIcon-read-only': Boolean(readOnly),
+    }),
+    statusIcon: generateClassNames(stylesInput, {
+      endIcon: true,
+      [`endIcon-${size}`]: Boolean(size),
+      [`endIcon-${variant}`]: Boolean(variant),
+      'endIcon-disabled': Boolean(disabled),
+      'endIcon-inValid': isError === true,
+      'endIcon-valid': isError === false,
+    }),
   };
 };
 
 export const getClassNamesInput: GetClassNamesInput = (params) => {
   const { size = 'size-sm', isError, variant = 'basic', disabled } = params;
   return {
-    fieldset: classNames(
-      stylesInput.fieldset,
-      generateClassNames(stylesInput, {
-        [`fieldset-${size}`]: Boolean(size),
-        [`fieldset-${variant}`]: Boolean(variant),
-        'fieldset-disabled': Boolean(disabled),
-      })
-    ),
-    input: classNames(
-      stylesInput.input,
-      generateClassNames(stylesInput, {
-        [`input-${size}`]: Boolean(size),
-        [`input-${variant}`]: Boolean(variant),
-        'input-inValid': isError === true,
-        'input-valid': isError === false,
-      })
-    ),
-    legend: classNames(
-      stylesInput.legend,
-      generateClassNames(stylesInput, {
-        [`legend-${size}`]: Boolean(size),
-        [`legend-${variant}`]: Boolean(variant),
-        'legend-inValid': isError === true,
-        'legend-valid': isError === false,
-      })
-    ),
-    textarea: classNames(
-      stylesInput.textarea,
-      generateClassNames(stylesInput, {
-        [`textarea-${size}`]: Boolean(size),
-        [`textarea-${variant}`]: Boolean(variant),
-        'textarea-inValid': isError === true,
-        'textarea-valid': isError === false,
-      })
-    ),
+    fieldset: generateClassNames(stylesInput, {
+      fieldset: true,
+      [`fieldset-${size}`]: Boolean(size),
+      [`fieldset-${variant}`]: Boolean(variant),
+      'fieldset-disabled': Boolean(disabled),
+    }),
+    input: generateClassNames(stylesInput, {
+      input: true,
+      [`input-${size}`]: Boolean(size),
+      [`input-${variant}`]: Boolean(variant),
+      'input-inValid': isError === true,
+      'input-valid': isError === false,
+    }),
+    legend: generateClassNames(stylesInput, {
+      legend: true,
+      [`legend-${size}`]: Boolean(size),
+      [`legend-${variant}`]: Boolean(variant),
+      'legend-inValid': isError === true,
+      'legend-valid': isError === false,
+    }),
+    textarea: generateClassNames(stylesInput, {
+      textarea: true,
+      [`textarea-${size}`]: Boolean(size),
+      [`textarea-${variant}`]: Boolean(variant),
+      'textarea-inValid': isError === true,
+      'textarea-valid': isError === false,
+    }),
   };
 };
