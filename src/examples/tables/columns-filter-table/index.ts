0fca32d57c27b65eb8c8befe41fc9ff0e32c523d
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-07-27 14:25:15 +0200

    Add types to table filter sort example

--- src/examples/tables/columns-filter-table/index.ts
+++ src/examples/tables/columns-filter-table/index.ts
@@ -1,1 +1,2 @@
 export * from './ColumnsFilterTable';
+export * from './types';
---
+++ src/examples/tables/columns-filter-table/types.ts
@@ -0,0 +1,5 @@
+import { Table } from '@tanstack/react-table';
+
+export type ColumnsFilterTableProps<T extends object> = {
+  children?: (table: Table<T>) => React.ReactNode;
+};
