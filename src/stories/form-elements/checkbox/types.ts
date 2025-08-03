4902e49474174ef69ee351da8264c909b95cb11a
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-03 13:40:40 +0200

    Change name type for variants checkbox

--- src/stories/form-elements/checkbox/types.ts
+++ src/stories/form-elements/checkbox/types.ts
@@ -1,12 +1,12 @@
 import { Color, Size } from '@src/stories/types';
 import { InputHTMLAttributes } from 'react';
 
-export type Variant = 'filled' | 'unfilled';
+export type VariantCheckbox = 'filled' | 'unfilled';
 
 export interface CheckboxProps
   extends Omit<InputHTMLAttributes<HTMLInputElement>, 'size'> {
   chilren?: React.ReactNode;
   color?: Color;
   size?: Size;
-  variant?: Variant;
+  variant?: VariantCheckbox;
 }
