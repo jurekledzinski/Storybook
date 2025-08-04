ada709c3082a10a8185585cc358c45fc6dbc4a33
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-04 15:11:24 +0200

    Refactor container and connect styles with global space module css

--- src/stories/layout/container/utils/classNames.ts
+++ src/stories/layout/container/utils/classNames.ts
@@ -1,11 +1,14 @@
 import styles from '../Container.module.css';
+import stylesSpace from '@src/stories/styles/space.module.css';
 import { ClassesContainer } from './types';
 import { generateClassNames } from '@src/stories/helpers';
 
 export const getClassesContainer: ClassesContainer = (params) => {
   const { margin, maxWidth, padding } = params;
 
-  return generateClassNames(styles, {
+  const mergedClasses = { ...styles, ...stylesSpace };
+
+  return generateClassNames(mergedClasses, {
     container: true,
     [`${margin}`]: Boolean(margin),
     [`${maxWidth}`]: Boolean(maxWidth),
