e12c217ebe746fe9c0f7bfb359a888520bc9ae60
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-08 22:20:34 +0200

    Add select to app file for test fix bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
+import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
 import { Container, Header } from './app-ui';
-import { SimpleTable } from './examples/tables/custom';
+import {
+  Select,
+  SelectList,
+  SelectOption,
+  SelectPanel,
+  SelectTrigger,
+} from './stories/form-elements/select';
 import './stories/main.css';
 
 const App = () => {
   return (
     <Container padding="padding-xs">
       <Header />
-      <SimpleTable />
+      <div style={{ marginTop: 400 }}></div>
+      <Select>
+        <SelectTrigger
+          endIcon={[faChevronUp, faChevronDown]}
+          placeholder="Select color"
+        />
+        <SelectPanel>
+          <SelectList>
+            <SelectOption id="red">Red</SelectOption>
+            <SelectOption id="green">Green</SelectOption>
+            <SelectOption id="orange">Orange</SelectOption>
+            <SelectOption id="blue">Blue</SelectOption>
+            <SelectOption id="yellow">Yellow</SelectOption>
+            <SelectOption id="purple">Purple</SelectOption>
+          </SelectList>
+        </SelectPanel>
+      </Select>
     </Container>
   );
 };
