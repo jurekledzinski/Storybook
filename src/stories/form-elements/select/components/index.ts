be4b16bfd2141cbe8f96d382307a023cf32765e3
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-09 12:59:45 +0200

    Comment idea of re export select after improvments code

--- src/stories/form-elements/select/components/index.ts
+++ src/stories/form-elements/select/components/index.ts
@@ -2,3 +2,15 @@ export * from './select-list';
 export * from './select-option';
 export * from './select-panel';
 export * from './select-trigger';
+
+// import { SelectList } from './select-list';
+// import { SelectOption } from './select-option';
+// import { SelectPanel } from './select-panel';
+// import { SelectTrigger } from './select-trigger';
+
+// export const SelectComponents = {
+//   list: SelectList,
+//   option: SelectOption,
+//   panel: SelectPanel,
+//   trigger: SelectTrigger,
+// };
--- src/stories/form-elements/select/index.ts
+++ src/stories/form-elements/select/index.ts
@@ -4,3 +4,11 @@ export * from './Select';
 export * from './store';
 export * from './types';
 export * from './utils';
+
+// import { Select as SelectElement } from './Select';
+// import { SelectComponents } from './components';
+
+// export const Select = {
+//   container: SelectElement,
+//   ...SelectComponents,
+// };
