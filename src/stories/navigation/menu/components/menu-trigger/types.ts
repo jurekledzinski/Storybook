908297d5da540d973ae84a4377acb579fc4fbbe6
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-13 09:22:02 +0200

    Remove unnecessary parentId prop from menu trigger component

--- src/stories/navigation/menu/components/menu-trigger/types.ts
+++ src/stories/navigation/menu/components/menu-trigger/types.ts
@@ -3,7 +3,6 @@ import { ButtonHTMLAttributes } from 'react';
 export interface MenuTriggerProps
   extends ButtonHTMLAttributes<HTMLButtonElement> {
   fullWidth?: boolean;
-  parentId?: string;
 }
 
 export type GetClassNamesMenuTrigger = (fullWidth?: boolean) => string;
