d6614d7569320a8daec576f4e5ce0c034fced60a
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-16 21:05:54 +0200

    Refactor Box component

--- src/stories/layout/box/Box.tsx
+++ src/stories/layout/box/Box.tsx
@@ -1,9 +1,15 @@
 import { BoxProps } from './types';
-import { getClassesBox, getInlineBoxStyles } from './utils';
+import { getClassesBox } from './utils';
+import {
+  filterProps,
+  spacingInlineStyles,
+  spacingValues,
+} from '@src/stories/helpers';
 
 export const Box = ({ children, ...props }: BoxProps) => {
   const classes = getClassesBox(props);
-  const inline = getInlineBoxStyles(props);
+  const spacingProps = filterProps(props, spacingValues, true);
+  const inline = spacingInlineStyles(spacingProps);
 
   return (
     <div {...props} className={classes} style={inline}>
--- src/stories/layout/box/utils/classNames.ts
+++ src/stories/layout/box/utils/classNames.ts
@@ -1,23 +1,15 @@
 import stylesSpace from '@src/stories/styles/space.module.css';
 import { ClassesBox } from './types';
-import { classNames, generateClassNames } from '@src/stories/helpers';
+import {
+  classNames,
+  generateClassNames,
+  spacingClasses,
+} from '@src/stories/helpers';
 
 export const getClassesBox: ClassesBox = (params) => {
   const { className, m, mb, mt, ml, mr, p, pb, pl, pr, pt } = params;
 
-  return classNames(
-    generateClassNames(stylesSpace, {
-      [`${m}`]: Boolean(m),
-      [`${mb}`]: Boolean(mb),
-      [`${ml}`]: Boolean(ml),
-      [`${mr}`]: Boolean(mr),
-      [`${mt}`]: Boolean(mt),
-      [`${p}`]: Boolean(p),
-      [`${pb}`]: Boolean(pb),
-      [`${pl}`]: Boolean(pl),
-      [`${pr}`]: Boolean(pr),
-      [`${pt}`]: Boolean(pt),
-    }),
-    className ?? ''
-  );
+  const spacing = spacingClasses({ m, mb, ml, mr, mt, p, pb, pl, pr, pt });
+
+  return classNames(generateClassNames(stylesSpace, spacing), className ?? '');
 };
