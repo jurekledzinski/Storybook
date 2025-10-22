d6fc29cdd3ab03049b387757175b39d936c1f2b7
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-10-22 18:52:30 +0200

    Refactor helper function to generate classNames for Buttons

--- src/stories/buttons/button/utils/classNames.ts
+++ src/stories/buttons/button/utils/classNames.ts
@@ -1,24 +1,25 @@
 import styles from '../styles/Button.module.css';
-import { BaseButtonProps } from '../types';
-import { classNames } from '@src/stories/helpers';
+import { ButtonClassNames } from './types';
+import { generateClassNames } from '@src/stories/helpers';
 
-export const getClassButton = ({
+export const buttonClassNames: ButtonClassNames = ({
   border = 'border-xs',
   color = 'primary',
+  disabled,
+  fullWidth,
+  isLoading,
+  radius,
   size = 'size-md',
   variant = 'contained',
-  ...params
-}: Omit<BaseButtonProps, 'iconEnd' | 'iconStart' | 'label'>) => {
-  const defaultValues = { border, color, size, variant };
-  const stylesValues = Object.values({
-    ...params,
-    ...defaultValues,
+}) => {
+  return generateClassNames(styles, {
+    button: true,
+    [border]: Boolean(border),
+    [color]: Boolean(color),
+    [size]: Boolean(size),
+    [variant]: Boolean(variant),
+    [`${radius}`]: Boolean(radius),
+    disabled: Boolean(disabled || isLoading),
+    fullWidth: Boolean(fullWidth),
   });
-
-  return classNames(
-    styles.button,
-    ...stylesValues.map((key) => styles[key as keyof typeof styles]),
-    params.disabled || params.isLoading ? styles.disabled : '',
-    params.fullWidth ? styles.fullWidth : ''
-  );
 };
