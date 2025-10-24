ff206a901196e93fc1e151abc7483f539640ff94
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-10-24 18:51:35 +0200

    Update chip props

--- src/stories/display/chip/types.ts
+++ src/stories/display/chip/types.ts
@@ -1,16 +1,14 @@
 import { Color, Icon, Radius, Size, Variant } from '@src/stories/types';
 import { HTMLAttributes, MouseEventHandler } from 'react';
 
-type ChipVariant = Exclude<Variant, 'text'>;
-
 export interface ChipProps extends HTMLAttributes<HTMLDivElement> {
   color?: Color;
   deleteIcon?: Icon;
   label?: React.ReactNode;
+  onDelete?: MouseEventHandler<HTMLButtonElement>;
   radius?: Radius;
   size?: Size;
   startIcon?: Icon;
   startIconSrc?: string;
-  variant?: ChipVariant;
-  onDelete?: MouseEventHandler<HTMLButtonElement>;
+  variant?: Exclude<Variant, 'text'>;
 }
