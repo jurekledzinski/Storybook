2070d6f929f67a8b4b3c3cc5e5c03e34dfffef05
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-10-28 21:04:47 +0100

    Update color types for AlertProps

--- src/stories/feedbacks/alert/types.ts
+++ src/stories/feedbacks/alert/types.ts
@@ -3,7 +3,7 @@ import { Color, Radius } from '@src/stories/types';
 export type AlertProps = {
   children?: React.ReactNode;
   className?: string;
-  color?: Color | 'info';
+  color?: Color;
   fullWidth?: boolean;
   radius?: Radius;
   variant?: 'contained' | 'filled' | 'light' | 'outlined';
