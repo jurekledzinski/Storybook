ca92ff7826e0012360f795283b8ae98f116250de
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-10-28 18:52:56 +0100

    Add info color for IconButton component

--- src/stories/buttons/icon-button/types.ts
+++ src/stories/buttons/icon-button/types.ts
@@ -11,7 +11,7 @@ export type IconBaseButtonProps = {
   icon: Icons;
   border?: Border;
   className?: string;
-  color?: Color | 'white';
+  color?: Color;
   contrast?: boolean;
   disabled?: boolean;
   fullWidth?: boolean;
