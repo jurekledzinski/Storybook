cca8cd6db3b6614f26932655dfcb84a63ad763a8
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-10-28 22:42:36 +0100

    Update Message stories

--- src/stories/feedbacks/message/Message.stories.tsx
+++ src/stories/feedbacks/message/Message.stories.tsx
@@ -1,24 +1,39 @@
 import { Message } from './Message';
-import { Meta, StoryObj } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react-vite';
+import { Stack } from '@src/app-ui';
 
 const meta: Meta<typeof Message> = {
   component: Message,
   title: 'Components/Feedbacks/Message',
-  args: {},
+  args: { children: 'Message example' },
 };
 
 export default meta;
 
 type Story = StoryObj<typeof Message>;
 
-export const Default: Story = {
-  args: {
-    children: 'Example message',
-  },
+export const Playground: Story = {
   argTypes: {
     variant: {
-      control: { type: 'select' },
+      control: 'select',
       options: ['error', 'info', 'secondary', 'success', 'warning'],
+    },
+  },
+};
+
+export const Default: Story = {
+  render: (args) => (
+    <Stack orientation="column" spacing="normal">
+      <Message {...args} variant="error" />
+      <Message {...args} variant="info" />
+      <Message {...args} variant="secondary" />
+      <Message {...args} variant="success" />
+      <Message {...args} variant="warning" />
+    </Stack>
+  ),
+  parameters: {
+    controls: {
+      include: [],
     },
   },
 };
