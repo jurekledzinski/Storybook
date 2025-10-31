d2952ea3ea4bc5a10f95d851e0977ec122e61d45
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-10-31 20:10:38 +0100

    Add to InputWrapperProps isEndIcon, isStartIcon,as types

--- src/stories/form-elements/input/components/input-wrapper/types.ts
+++ src/stories/form-elements/input/components/input-wrapper/types.ts
@@ -1,11 +1,13 @@
 import { InputVariant, Size } from '@src/stories/types';
 
 export interface InputWrapperProps {
-  children?: React.ReactNode;
   as?: 'input' | 'textarea';
+  children?: React.ReactNode;
   disabled?: boolean;
+  isEndIcon?: boolean;
   isError?: boolean;
   isPending?: boolean;
+  isStartIcon?: boolean;
   readOnly?: boolean;
   size?: Size;
   variant?: InputVariant;
