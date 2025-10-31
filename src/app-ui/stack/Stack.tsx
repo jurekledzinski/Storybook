f844fc05b20746776b7dc8cadec61856f6d8cc3a
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-10-31 15:56:38 +0100

    Update stack component for app ui

--- src/app-ui/stack/Stack.tsx
+++ src/app-ui/stack/Stack.tsx
@@ -4,5 +4,9 @@ import { StackProps } from './types';
 export const Stack = ({ children, ...props }: StackProps) => {
   const classNames = stackClassNames(props);
 
-  return <div className={classNames}>{children}</div>;
+  return (
+    <div className={classNames} style={props.style}>
+      {children}
+    </div>
+  );
 };
