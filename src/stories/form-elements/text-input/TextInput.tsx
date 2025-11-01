355cdce0d47a1f9f73346c52eac30cb4a55ce4db
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-11-01 20:23:27 +0100

    Add Icon status loader start end to text input

--- src/stories/form-elements/text-input/TextInput.tsx
+++ src/stories/form-elements/text-input/TextInput.tsx
@@ -1,9 +1,16 @@
 import { forwardRef } from 'react';
-import { Input, InputWrapper } from '@src/stories/form-elements/input';
 import { InputProps } from './types';
+import {
+  IconEnd,
+  IconStart,
+  IconStatus,
+  Input,
+  InputLoader,
+  InputWrapper,
+} from '../input';
 
 export const TextInput = forwardRef<HTMLInputElement, InputProps>(
-  ({ endIcon, isPending, startIcon, ...props }, ref) => {
+  ({ endIcon, isPending, startIcon, onClick, ...props }, ref) => {
     return (
       <InputWrapper
         {...props}
@@ -12,7 +19,11 @@ export const TextInput = forwardRef<HTMLInputElement, InputProps>(
         isEndIcon={!!endIcon && Boolean(props.value)}
         isStartIcon={!!startIcon}
       >
+        {startIcon ? <IconStart icon={startIcon[0]} /> : null}
         <Input {...props} ref={ref} />
+        <InputLoader />
+        <IconStatus />
+        {endIcon ? <IconEnd icon={endIcon[0]} onClick={onClick} /> : null}
       </InputWrapper>
     );
   }
--- src/stories/form-elements/text-input/types.ts
+++ src/stories/form-elements/text-input/types.ts
@@ -1,12 +1,13 @@
 import { Icons, InputVariant, Size } from '@src/stories/types';
-import { InputHTMLAttributes } from 'react';
+import { InputHTMLAttributes, MouseEventHandler } from 'react';
 
 export interface InputProps
-  extends Omit<InputHTMLAttributes<HTMLInputElement>, 'size'> {
+  extends Omit<InputHTMLAttributes<HTMLInputElement>, 'size' | 'onClick'> {
   endIcon?: Icons;
   isError?: boolean;
   isPending?: boolean;
   label?: string;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
   size?: Size;
   startIcon?: Icons;
   variant?: InputVariant;
