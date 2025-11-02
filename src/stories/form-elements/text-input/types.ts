f4c41fd6b424f876ac4053e60699d5f09c9e58d9
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-11-02 17:41:31 +0100

    Change types endIcon and startIcon for TextInputProps

--- src/stories/form-elements/text-input/types.ts
+++ src/stories/form-elements/text-input/types.ts
@@ -1,14 +1,14 @@
-import { Icons, InputVariant, Size } from '@src/stories/types';
+import { Icon, InputVariant, Size } from '@src/stories/types';
 import { InputHTMLAttributes, MouseEventHandler } from 'react';
 
 export interface InputProps
   extends Omit<InputHTMLAttributes<HTMLInputElement>, 'size' | 'onClick'> {
-  endIcon?: Icons;
+  endIcon?: Icon;
   isError?: boolean;
   isPending?: boolean;
   label?: string;
   onClick?: MouseEventHandler<HTMLButtonElement>;
   size?: Size;
-  startIcon?: Icons;
+  startIcon?: Icon;
   variant?: InputVariant;
 }
