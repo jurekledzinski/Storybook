f18d925d376dc4c205127fea08064620f1c45dce
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-11-03 12:00:52 +0100

    Replace InputWrapper in SearchInput to ControlLayout

--- src/stories/form-elements/search-input/SearchInput.tsx
+++ src/stories/form-elements/search-input/SearchInput.tsx
@@ -1,33 +1,27 @@
+import { ControlLayout, IconEnd, IconStart } from '@src/stories/layout';
 import { faMagnifyingGlass, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { forwardRef } from 'react';
-import { IconEnd, IconStart, InputWrapper } from '../input/components';
 import { Input } from '../input';
 import { SearchInputProps } from './types';
 
 export const SearchInput = forwardRef<HTMLInputElement, SearchInputProps>(
   (
-    {
-      endIcon = faXmark,
-      label,
-      startIcon = faMagnifyingGlass,
-      isPending,
-      onClick,
-      ...props
-    },
+    { endIcon = faXmark, label, startIcon = faMagnifyingGlass, isPending, onClick, ...props },
     ref
   ) => {
     return (
-      <InputWrapper
+      <ControlLayout
+        {...props}
         as="input"
         isPending={isPending}
         isEndIcon={!!endIcon && Boolean(props.value)}
         isStartIcon={!!startIcon}
-        {...props}
+        isStatus={false}
       >
         <IconStart icon={startIcon} />
         <Input label={label} {...props} ref={ref} />
         {props.value ? <IconEnd icon={endIcon} onClick={onClick} /> : null}
-      </InputWrapper>
+      </ControlLayout>
     );
   }
 );
