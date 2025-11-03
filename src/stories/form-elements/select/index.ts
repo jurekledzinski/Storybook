fc27b58c48b4dcb331aff2bf42cfd6846c046eba
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-11-03 20:26:59 +0100

    Add Select styles

--- src/stories/form-elements/select/index.ts
+++ src/stories/form-elements/select/index.ts
@@ -1,14 +1,2 @@
-export * from './components';
-export * from './Select.stories';
 export * from './Select';
-export * from './store';
-export * from './types';
-export * from './utils';
-
-// import { Select as SelectElement } from './Select';
-// import { SelectComponents } from './components';
-
-// export const Select = {
-//   container: SelectElement,
-//   ...SelectComponents,
-// };
+export * from './components';
