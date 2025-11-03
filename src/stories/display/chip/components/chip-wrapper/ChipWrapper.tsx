43ca7ae75c5aaa8ec94c8a72e1e99d9e35323fd2
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-11-03 20:16:42 +0100

    Format code on save

--- src/stories/display/chip/components/chip-wrapper/ChipWrapper.tsx
+++ src/stories/display/chip/components/chip-wrapper/ChipWrapper.tsx
@@ -3,11 +3,12 @@ import { ChipWrapperProps } from './types';
 import { DeleteButton } from '../delete-button';
 import { Icon } from '@src/stories/graphics/icon';
 import { StartIcon } from '../start-icon';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 export const ChipWrapper = ({
   children,
   onDelete,
-  deleteIcon,
+  deleteIcon = faXmark,
   startIcon,
   startIconSrc,
   ...props
@@ -21,11 +22,7 @@ export const ChipWrapper = ({
       {children && <span className={classNames.label}>{children}</span>}
 
       {onDelete && deleteIcon ? (
-        <DeleteButton
-          className={classNames.delete}
-          id={props.id}
-          onDelete={onDelete}
-        >
+        <DeleteButton className={classNames.delete} id={props.id} onDelete={onDelete}>
           <Icon icon={deleteIcon} />
         </DeleteButton>
       ) : null}
