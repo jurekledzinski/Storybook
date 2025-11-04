932bc7fe39af17e56935b9f573e273c7f6e36de4
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-11-04 20:57:08 +0100

    Add stories to ControlLayout component and update Docs.mdx for it

--- src/stories/layout/control-layout/ControlLayout.stories.tsx
+++ src/stories/layout/control-layout/ControlLayout.stories.tsx
@@ -1,9 +1,12 @@
 import { ControlLayout } from './ControlLayout';
+import { faAt } from '@fortawesome/free-solid-svg-icons';
+import { IconEnd } from './components';
+import { Input } from '@src/stories/form-elements/input';
 import { Meta, StoryObj } from '@storybook/react-vite';
 
 const meta: Meta<typeof ControlLayout> = {
   component: ControlLayout,
-  title: 'Components/Display/ControlLayout',
+  title: 'Components/Layout/ControlLayout',
   args: {},
 };
 
@@ -12,5 +15,18 @@ export default meta;
 type Story = StoryObj<typeof ControlLayout>;
 
 export const Default: Story = {
+  render: (args) => (
+    <ControlLayout
+      {...args}
+      as="input"
+      isPending={false}
+      isEndIcon={true}
+      isStartIcon={false}
+      isStatus={false}
+    >
+      <Input />
+      <IconEnd icon={faAt} />
+    </ControlLayout>
+  ),
   parameters: { controls: { disable: true } },
 };
