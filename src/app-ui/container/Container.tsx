10ac507463930b4b5cf2906eb902a7181711cbbe
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-11-07 13:24:45 +0100

    Add Div element default props to container app ui elements

--- src/app-ui/container/Container.tsx
+++ src/app-ui/container/Container.tsx
@@ -1,11 +1,12 @@
 import { ContainerProps } from './types';
 import { getClassesContainer } from './utils';
 
-export const Container = ({
-  children,
-  padding = 'padding-sm',
-}: ContainerProps) => {
+export const Container = ({ children, padding = 'padding-sm', ...props }: ContainerProps) => {
   const classes = getClassesContainer(padding);
 
-  return <div className={classes}>{children}</div>;
+  return (
+    <div {...props} className={classes}>
+      {children}
+    </div>
+  );
 };
