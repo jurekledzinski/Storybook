cfa01f261b04d06f9dbe8393e6a0010ebbf0d0c0
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-11-07 20:52:46 +0100

    Update App file to default

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
 import { Container, Header } from './app-ui';
-import { Button, Menu, MenuItem, MenuTrigger } from './stories';
 import './stories/styles/main.css';
 
 const App = () => {
   return (
     <Container style={{ minHeight: '200vh' }} padding="padding-xs">
       <Header />
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <MenuTrigger>
-          <Button label="Services" />
-          <Menu>
-            <MenuItem>Ser</MenuItem>
-            <MenuItem>Ser</MenuItem>
-            <MenuItem>Ser</MenuItem>
-            <MenuItem>Ser</MenuItem>
-          </Menu>
-        </MenuTrigger>
-      </div>
     </Container>
   );
 };
