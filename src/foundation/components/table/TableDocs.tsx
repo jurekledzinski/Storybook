0664c2e5b4cf56a15ab5d912fc0a1d40c890f666
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-11-11 14:26:07 +0100

    Add wrapper div around table docs to show scroll

--- src/foundation/components/table/TableDocs.tsx
+++ src/foundation/components/table/TableDocs.tsx
@@ -3,31 +3,33 @@ import { colorsData, headerData } from '../utils';
 
 export const TableDocs = () => {
   return (
-    <table className={styles.table}>
-      <thead className={styles.thead}>
-        <tr className={styles.tr}>
-          {headerData.map((item) => (
-            <th className={styles.th} key={item} scope="col">
-              {item}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {colorsData.map((item) => (
-          <tr key={item.hue} className={styles.tr}>
-            <td className={styles.td} scope="row">
-              {item.hue}
-            </td>
-            <td className={styles.td}>{item.color}</td>
-            <td className={styles.td}>{item.hsl}</td>
-            <td className={styles.td}>{item.hex}</td>
-            <td className={styles.td}>
-              <span className={styles.sample} style={{ backgroundColor: item.hex }}></span>
-            </td>
+    <div className={styles.container}>
+      <table className={styles.table}>
+        <thead className={styles.thead}>
+          <tr className={styles.tr}>
+            {headerData.map((item) => (
+              <th className={styles.th} key={item} scope="col">
+                {item}
+              </th>
+            ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {colorsData.map((item) => (
+            <tr key={item.hue} className={styles.tr}>
+              <td className={styles.td} scope="row">
+                {item.hue}
+              </td>
+              <td className={styles.td}>{item.color}</td>
+              <td className={styles.td}>{item.hsl}</td>
+              <td className={styles.td}>{item.hex}</td>
+              <td className={styles.td}>
+                <span className={styles.sample} style={{ backgroundColor: item.hex }}></span>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
