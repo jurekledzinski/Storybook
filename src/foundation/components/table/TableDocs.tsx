d24f0707493baa7b4aac59afb18a2149b59cef16
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-11-11 14:10:18 +0100

    Create table for display colors range in Colors.mdx

---
+++ src/foundation/components/table/TableDocs.tsx
@@ -0,0 +1,33 @@
+import styles from './TableDocs.module.css';
+import { colorsData, headerData } from '../utils';
+
+export const TableDocs = () => {
+  return (
+    <table className={styles.table}>
+      <thead className={styles.thead}>
+        <tr className={styles.tr}>
+          {headerData.map((item) => (
+            <th className={styles.th} key={item} scope="col">
+              {item}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {colorsData.map((item) => (
+          <tr key={item.hue} className={styles.tr}>
+            <td className={styles.td} scope="row">
+              {item.hue}
+            </td>
+            <td className={styles.td}>{item.color}</td>
+            <td className={styles.td}>{item.hsl}</td>
+            <td className={styles.td}>{item.hex}</td>
+            <td className={styles.td}>
+              <span className={styles.sample} style={{ backgroundColor: item.hex }}></span>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
